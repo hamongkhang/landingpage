7c4eeb0de9aac62dbb2de931af92e2942ef0ce3a
hamongkhang <khang.ha22@student.passerellesnumeriques.org>
2021-07-01 16:19:28 +0700

    User Interface

---
+++ src/components/FAQ.js
@@ -0,0 +1,161 @@
+const FAQ=()=>{
+    return(
+    <section id="faq" className="faq section-bg">
+    <div className="container">
+      <div className="section-title" data-aos="fade-up">
+        <h2>
+      Tuyển dụng
+        </h2>
+        <p>Đội ngũ nhân viên tài năng và tâm huyết là chìa khóa để chúng tôi phát triển các dự án và duy trì các hoạt động hiệu quả.</p>
+      </div>
+      <div className="faq-list">
+
+      Nếu bạn đang tìm kiếm thử thách mới, hãy tham gia cùng chúng tôi. PN thường xuyên cần nguồn nhân lực cho các vị trí nhân viên dự án, tình nguyện viên, và thực tập sinh tại trụ sở chính tại Pháp, hoặc tại một trong các trung tâm tại Campuchia, Philippines và Việt Nam, cũng như các chi nhánh của chúng tôi tại Singapore và Hồng Kông.
+
+Các vị trí đang tuyển dụng cho trung tâm PN tại Việt Nam. Xem thêm cơ hội nghề nghiệp của các trung tâm khác tại đây, bản tiếng Anh hoặc bản tiếng Pháp.
+
+Chúng tôi thường xuyên tìm kiếm tình nguyện viên và thực tập sinh ( truyền thông, gây quỹ, tài chính, etc.). Bạn có thể nộp ngay hồ sơ bằng cách gửi CV và cover letter đến: missions@passerellesnumeriques.org.
+        <br />
+        <ul>
+          <li data-aos="fade-up">
+            <i className="bx bx-help-circle icon-help" /> <a data-toggle="collapse" className="collapse" href="#faq-list-1"><h3>PN Vietnam – Educator (Local Contract)</h3>
+<p>Th2 23, 2021 | Cùng tham gia, Việt Nam
+</p><i className="bx bx-chevron-down icon-show" /><i className="bx bx-chevron-up icon-close" /></a>
+            <div id="faq-list-1" className="collapse show" data-parent=".faq-list">
+              <p>
+              Background
+Passerelles numériques (PN), a non-profit organisation, is dedicated to provide education, technical and professional training in the digital sector to young underprivileged people by leveraging their potential and willpower. We endeavour to truly develop their employability which will allow them and their families to escape poverty in a sustainable way, and contribute to the social and economic development of their countries.
+
+Launched in 2010 in Da Nang, PN Vietnam offers a 3-year vocational training program to youth in precarious situations from the Central region of Vietnam. PN Vietnam has been supporting almost 500 disadvantaged students so far.
+
+Website: http://www.passerellesnumeriques.org/en
+
+Context and mission
+Train students in IT subjects (both front & back-end development)
+
+Duties and Responsibilities
+Teach (in Vietnamese) from 12 to 18 hours per week to various level students.
+Work in alignment with IT syllabus and PN educational standards.
+Prepare, compile, and adjust training materials in accord with students’ learning style.
+Mark periodic tests and complete soft files of results for record.
+Monitor the performance of in-charge students and report to the Training manager about their progress.
+Participate in the student’s more broad, non-IT related education.
+Skills and experience
+EDUCATION
+
+Level of education: Bachelor
+Specialty: IT development
+PROFESSIONAL EXPERIENCE
+
+Experience length: 1 or 2 years
+Field: IT, Teacher
+SKILLS
+
+Job Specific Knowledge, Skills and Abilities
+
+IT Development
+English Communication Skills
+Team player
+Time management
+Positive attitude
+Flexibility/Adaptability
+Patience
+Pedagogical skills
+Experience in mobile development is a plus (Android, ReactNative, Hybrid)
+Experience with Python is a plus
+Experience with Databases (SQL, Firebase)
+IT Skills
+
+Understanding of Object Oriented Design
+JavaScript ES6
+PHP, Laravel framework
+React.js or comparable js libraries
+Aware of software development life cycle and team development settings
+LANGUAGES
+
+English, Vietnamese
+Status and conditions
+Salary depending on profile
+Insurances by Vietnamese government’s rules
+Private Health care Insurance
+Paid leave: 20 days per year
+Holidays bonus
+Senior bonus every 3 years
+Annual bonus
+  </p>
+            </div>
+          </li>
+          <li data-aos="fade-up" data-aos-delay={100}>
+            <i className="bx bx-help-circle icon-help" /> <a data-toggle="collapse" href="#faq-list-2" className="collapsed"><h3>PN Vietnam – IT Trainer (Local Contract)</h3>
+Th1 15, 2021 | Cùng tham gia, Việt Nam<i className="bx bx-chevron-down icon-show" /><i className="bx bx-chevron-up icon-close" /></a>
+            <div id="faq-list-2" className="collapse" data-parent=".faq-list">
+              <p>
+              Background
+Passerelles numériques (PN), a non-profit organisation, is dedicated to provide education, technical and professional training in the digital sector to young underprivileged people by leveraging their potential and willpower. We endeavour to truly develop their employability which will allow them and their families to escape poverty in a sustainable way, and contribute to the social and economic development of their countries.
+
+Launched in 2010 in Da Nang, PN Vietnam offers a 3-year vocational training program to youth in precarious situations from the Central region of Vietnam. PN Vietnam has been supporting almost 500 disadvantaged students so far.
+
+Website: http://www.passerellesnumeriques.org/en
+
+Context and mission
+Train students in IT subjects (both front & back-end development)
+
+Duties and Responsibilities
+Teach (in Vietnamese) from 12 to 18 hours per week to various level students.
+Work in alignment with IT syllabus and PN educational standards.
+Prepare, compile, and adjust training materials in accord with students’ learning style.
+Mark periodic tests and complete soft files of results for record.
+Monitor the performance of in-charge students and report to the Training manager about their progress.
+Participate in the student’s more broad, non-IT related education.
+Skills and experience
+EDUCATION
+
+Level of education: Bachelor
+Specialty: IT development
+PROFESSIONAL EXPERIENCE
+
+Experience length: 1 or 2 years
+Field: IT, Teacher
+SKILLS
+
+Job Specific Knowledge, Skills and Abilities
+
+IT Development
+English Communication Skills
+Team player
+Time management
+Positive attitude
+Flexibility/Adaptability
+Patience
+Pedagogical skills
+Experience in mobile development is a plus (Android, ReactNative, Hybrid)
+Experience with Python is a plus
+Experience with Databases (SQL, Firebase)
+IT Skills
+
+Understanding of Object Oriented Design
+JavaScript ES6
+PHP, Laravel framework
+React.js or comparable js libraries
+Aware of software development life cycle and team development settings
+LANGUAGES
+
+English, Vietnamese
+Status and conditions
+Salary depending on profile
+Insurances by Vietnamese government’s rules
+Private Health care Insurance
+Paid leave: 20 days per year
+Holidays bonus
+Senior bonus every 3 years
+Annual bonus
+               </p>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </section>
+    );
+}
+export default FAQ;
